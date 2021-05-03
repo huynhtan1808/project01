52c52d905c7095863c7f77cb5e57dfe2e1aabdb7
huynhtan1808 <tanhuynh1808@gmail.com>
2021-05-04 00:17:09 +0700

    Deploy My 1st React App to Github

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/pages/HomePage/Home';
+import Works from './components/pages/Works/Works';
+import Footer from './components/pages/Footer/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/myworks' component={Works} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Works/Works.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSection from '../../HeroSection';
+import Pricing from '../../Pricing';
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
+
+function Home() {
+  return (
+    <>    
+      <Pricing />
+    </>
+  );
+}
+
+export default Home;

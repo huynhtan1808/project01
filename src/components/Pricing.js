9e42ddec2c2f3912ea97fc2812d19bed7452e389
huynhtan1808 <tanhuynh1808@gmail.com>
2021-05-06 00:20:58 +0700

    Footer

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -16,6 +16,34 @@ function Pricing() {
           <td className="time"><a target="_blank" href="/remote-jobs/103856-remote-senior-frontend-engineer-vaan-group"><time datetime="2021-05-05T13:49:31+00:00">📌	2h</time></a></td>
           <td className="source"><a rel="noindex nofollow" class="no-border tooltip" target="_blank" href="/l/103856"><div target="_blank" class="apply_button no-logo"><p>Apply</p></div></a></td>
         </tr>
+        <tr>
+          <td><img src={logo1} alt=""/></td>
+          <td className="company company_and_position"><h3>Vaan Group</h3></td>
+          <td className="tags"><div rel="follow" className="tag"><h3>react</h3></div></td>
+          <td className="time"><a target="_blank" href="/remote-jobs/103856-remote-senior-frontend-engineer-vaan-group"><time datetime="2021-05-05T13:49:31+00:00">📌	2h</time></a></td>
+          <td className="source"><a rel="noindex nofollow" class="no-border tooltip" target="_blank" href="/l/103856"><div target="_blank" class="apply_button no-logo"><p>Apply</p></div></a></td>
+        </tr>
+        <tr>
+          <td><img src={logo1} alt=""/></td>
+          <td className="company company_and_position"><h3>Vaan Group</h3></td>
+          <td className="tags"><div rel="follow" className="tag"><h3>react</h3></div></td>
+          <td className="time"><a target="_blank" href="/remote-jobs/103856-remote-senior-frontend-engineer-vaan-group"><time datetime="2021-05-05T13:49:31+00:00">📌	2h</time></a></td>
+          <td className="source"><a rel="noindex nofollow" class="no-border tooltip" target="_blank" href="/l/103856"><div target="_blank" class="apply_button no-logo"><p>Apply</p></div></a></td>
+        </tr>
+        <tr>
+          <td><img src={logo1} alt=""/></td>
+          <td className="company company_and_position"><h3>Vaan Group</h3></td>
+          <td className="tags"><div rel="follow" className="tag"><h3>react</h3></div></td>
+          <td className="time"><a target="_blank" href="/remote-jobs/103856-remote-senior-frontend-engineer-vaan-group"><time datetime="2021-05-05T13:49:31+00:00">📌	2h</time></a></td>
+          <td className="source"><a rel="noindex nofollow" class="no-border tooltip" target="_blank" href="/l/103856"><div target="_blank" class="apply_button no-logo"><p>Apply</p></div></a></td>
+        </tr>
+        <tr>
+          <td><img src={logo1} alt=""/></td>
+          <td className="company company_and_position"><h3>Vaan Group</h3></td>
+          <td className="tags"><div rel="follow" className="tag"><h3>react</h3></div></td>
+          <td className="time"><a target="_blank" href="/remote-jobs/103856-remote-senior-frontend-engineer-vaan-group"><time datetime="2021-05-05T13:49:31+00:00">📌	2h</time></a></td>
+          <td className="source"><a rel="noindex nofollow" class="no-border tooltip" target="_blank" href="/l/103856"><div target="_blank" class="apply_button no-logo"><p>Apply</p></div></a></td>
+        </tr>
       </tbody>    
     </table> 
   );
